aaf22e98dc726236e0bda6ef371eee027d1d9fe6
Yurii Denysenko <yuridevx@gmail.com>
2019-04-09 18:14:37 +0300

    Before rewrite

--- src/main/java/dev/yurii/vk/polygon/api/MeController.java
+++ src/main/java/dev/yurii/vk/polygon/api/MeController.java
@@ -1,18 +1,25 @@
 package dev.yurii.vk.polygon.api;
 
+import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.security.Principal;
+import javax.inject.Provider;
 
 @RestController
 public class MeController {
 
+    @Autowired
+    private VkApiClient vk;
 
-    @RequestMapping("/me")
-    public Object user(OAuth2AuthenticationToken user) {
-        return user;
+    @Autowired
+    private Provider<UserActor> user;
+
+    @RequestMapping("/groups")
+    public Object user(OAuth2AuthenticationToken token) {
+        return vk.groups().get(user.get());
     }
 }
--- src/main/java/dev/yurii/vk/polygon/vkauth/VkConfiguration.java
+++ src/main/java/dev/yurii/vk/polygon/vkauth/VkConfiguration.java
@@ -1,19 +1,24 @@
 package dev.yurii.vk.polygon.vkauth;
 
 import com.vk.api.sdk.client.VkApiClient;
+import com.vk.api.sdk.client.actors.UserActor;
 import com.vk.api.sdk.httpclient.HttpTransportClient;
-import dev.yurii.vk.polygon.vkauth.converter.UserServiceRequestConverter;
 import dev.yurii.vk.polygon.vkauth.converter.TokenClientRequestConverter;
 import dev.yurii.vk.polygon.vkauth.converter.TokenClientResponseConverter;
+import dev.yurii.vk.polygon.vkauth.converter.UserServiceRequestConverter;
 import dev.yurii.vk.polygon.vkauth.converter.VkOauthUserService;
 import lombok.RequiredArgsConstructor;
 import lombok.var;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.security.oauth2.client.endpoint.DefaultAuthorizationCodeTokenResponseClient;
 import org.springframework.security.oauth2.client.http.OAuth2ErrorResponseErrorHandler;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
@@ -25,6 +30,7 @@ import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
 
 import java.util.Arrays;
 
@@ -38,14 +44,6 @@ public class VkConfiguration extends WebSecurityConfigurerAdapter {
     private TokenClientRequestConverter tokenClientRequestConverter = new TokenClientRequestConverter();
 
     private TokenClientResponseConverter tokenClientResponseConverter = new TokenClientResponseConverter();
-
-    @Bean
-    public VkApiClient createClient() {
-        var transport = new HttpTransportClient();
-        var client = new VkApiClient(transport);
-
-        return client;
-    }
 
     //TODO Implement better userInfoEndpoint error handler for better stability
 
@@ -87,6 +85,25 @@ public class VkConfiguration extends WebSecurityConfigurerAdapter {
         accessTokenResponseClient.setRestOperations(restTemplate);
 
         return accessTokenResponseClient;
+    }
+
+    @Bean
+    public VkApiClient client() {
+        var transport = new HttpTransportClient();
+        return new VkApiClient(transport);
+    }
+
+
+    @Bean
+    @Scope(WebApplicationContext.SCOPE_REQUEST)
+    public UserActor userActor(OAuth2AuthorizedClientService authorizedClientService) {
+        try {
+            OAuth2AuthenticationToken authToken = (OAuth2AuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+            var accessToken = authorizedClientService.loadAuthorizedClient(authToken.getAuthorizedClientRegistrationId(), authToken.getName());
+            return new UserActor((Integer) authToken.getPrincipal().getAttributes().get("id"), accessToken.getAccessToken().getTokenValue());
+        } catch (ClassCastException ex) {
+            return null;
+        }
     }
 
 
